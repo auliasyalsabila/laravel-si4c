54089d750000272f0da2f3219c5605f605a0b354
auliasyalsabila <auliasyalsabilaramadhani_2428240155@mhs.mdp.ac.id>
2026-04-28 12:04:17 +0700

    Initial commit

---
+++ laravel-si4c/app/Http/Controllers/FakultasController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\fakultas;
+use Illuminate\Http\Request;
+
+class FakultasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // akses model fakultas untuk mengambil semua data fakultas
+        $result = fakultas::all(); //sm kayak select * from fakultas
+        dd($result); //dumbdata untuk cek data yang diambil dari model fakultas
+        // kirim data fakultas ke view index
+        return view('fakultas.index', compact('result')); //compact untuk mengirim data ke view index
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(fakultas $fakultas)
+    {
+        //
+    }
+}
